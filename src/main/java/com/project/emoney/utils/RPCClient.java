739a84ce99aa674a41475345ee79abde8691af1e
Alvin <alvin.farkhan@gmail.com>
2020-05-11 09:47:49 +0700

    rabbitMQ

---
+++ src/main/java/com/project/emoney/utils/RPCClient.java
@@ -0,0 +1,63 @@
+package com.project.emoney.utils;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+@Component
+@NoArgsConstructor
+public class RPCClient implements AutoCloseable {
+
+  private Connection connection;
+  private Channel channel;
+  private String requestQueueName;
+
+  public RPCClient(String requestQueueName) throws IOException, TimeoutException {
+    this.requestQueueName = requestQueueName;
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    connection = factory.newConnection();
+    channel = connection.createChannel();
+  }
+
+  public String call(String message) throws IOException, InterruptedException {
+    final String corrId = UUID.randomUUID().toString();
+
+    String replyQueueName = channel.queueDeclare().getQueue();
+    AMQP.BasicProperties props = new AMQP.BasicProperties
+        .Builder()
+        .correlationId(corrId)
+        .replyTo(replyQueueName)
+        .build();
+
+    channel.basicPublish("", requestQueueName, props, message.getBytes(StandardCharsets.UTF_8));
+
+    final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+    String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+      if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+        response.offer(new String(delivery.getBody(), StandardCharsets.UTF_8));
+      }
+    }, consumerTag -> {
+    });
+
+    String result = response.take();
+    channel.basicCancel(ctag);
+    return result;
+  }
+
+  public void close() throws IOException {
+    connection.close();
+  }
+
+}
