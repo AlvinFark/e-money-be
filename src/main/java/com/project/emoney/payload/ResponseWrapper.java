a2c367e26d0d1f3a054ed676f49aec1cd0435ef5
Alvin <alvin.farkhan@gmail.com>
2020-05-11 09:04:14 +0700

    response wrapper

---
+++ src/main/java/com/project/emoney/payload/ResponseWrapper.java
@@ -0,0 +1,14 @@
+package com.project.emoney.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseWrapper {
+  int code;
+  String message;
+  Object data;
+}
