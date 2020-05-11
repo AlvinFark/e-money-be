ffbd891d21be0417e6f615d7d1849150e9663291
Alvin <alvin.farkhan@gmail.com>
2020-05-11 08:45:39 +0700

    user model

---
+++ src/main/java/com/project/emoney/entity/User.java
@@ -0,0 +1,17 @@
+package com.project.emoney.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+  long id;
+  String name;
+  String email;
+  String password;
+  long balance;
+  boolean active;
+}
