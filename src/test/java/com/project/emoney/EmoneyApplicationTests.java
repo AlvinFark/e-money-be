1e51bdb1f6e4f0c61f1cba828d955f1e1e83c32e
Alvin <alvin.farkhan@gmail.com>
2020-05-11 08:35:42 +0700

    init spring

---
+++ src/test/java/com/project/emoney/EmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.emoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoneyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
