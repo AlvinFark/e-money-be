234d647bc32a5a38518bc440f9e41ac6ece5cd30
Alvin <alvin.farkhan@gmail.com>
2020-05-11 08:55:55 +0700

    verification models

---
+++ src/main/java/com/project/emoney/entity/OTP.java
@@ -0,0 +1,17 @@
+package com.project.emoney.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OTP {
+  long id;
+  long userId;
+  String code;
+  LocalDateTime time;
+}
