337e406cdc761c796275ec9e141eb17f1109f6ee
Alvin <alvin.farkhan@gmail.com>
2020-05-12 11:18:15 +0700

    JWT

---
+++ src/main/java/com/project/emoney/security/JwtUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.project.emoney.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class JwtUserDetailsService implements UserDetailsService {
+  @Override
+  public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    return new User(s, "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
+  }
+}
