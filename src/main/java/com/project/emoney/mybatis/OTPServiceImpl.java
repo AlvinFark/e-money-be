e6d91603e0426d2420c3b8174ca91c7cf61613fd
Alvin <alvin.farkhan@gmail.com>
2020-05-12 22:24:27 +0700

    OTP

---
+++ src/main/java/com/project/emoney/mybatis/OTPServiceImpl.java
@@ -0,0 +1,18 @@
+package com.project.emoney.mybatis;
+
+import com.project.emoney.entity.OTP;
+import com.project.emoney.mapper.OTPMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OTPServiceImpl implements OTPService {
+
+  @Autowired
+  OTPMapper otpMapper;
+
+  @Override
+  public void create(OTP otp) {
+    otpMapper.create(otp);
+  }
+}
