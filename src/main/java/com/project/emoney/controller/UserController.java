2f18359106d4d02481e3a5f2503d4f5a89a23325
Alvin <alvin.farkhan@gmail.com>
2020-05-12 16:47:12 +0700

    harusnnya punya reload profile

--- src/main/java/com/project/emoney/controller/UserController.java
+++ src/main/java/com/project/emoney/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.project.emoney.controller;
 
-import com.project.emoney.entity.User;
 import com.project.emoney.mybatis.UserService;
+import com.project.emoney.payload.ResponseWrapper;
 import com.project.emoney.payload.SimpleResponseWrapper;
+import com.project.emoney.security.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +18,9 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/profile")
-    public ResponseEntity<?> loadProfile(@RequestHeader("Authorization") String token){
-        return new ResponseEntity<>(new SimpleResponseWrapper(201, token), HttpStatus.OK);
+    public ResponseEntity<?> loadProfile(@CurrentUser User userDetails){
+//        com.project.emoney.entity.User user = userService.ge
+        return new ResponseEntity<>(new ResponseWrapper(201, "success", userDetails), HttpStatus.OK);
     }
 
 }
