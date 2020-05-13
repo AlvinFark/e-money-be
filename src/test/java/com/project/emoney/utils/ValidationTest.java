cbc7075aa7c745782c7d3fb3ef0e0a5f9793e229
Alvin <alvin.farkhan@gmail.com>
2020-05-13 20:51:03 +0700

    base test

---
+++ src/test/java/com/project/emoney/utils/ValidationTest.java
@@ -0,0 +1,12 @@
+package com.project.emoney.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ValidationTest {
+
+  @Test
+  public void validEmailTest() {
+  }
+}
