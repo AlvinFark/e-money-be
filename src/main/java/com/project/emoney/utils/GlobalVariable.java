c06c9fc371997698b89e935626d6b4cc9d6ef159
Alvin <alvin.farkhan@gmail.com>
2020-05-19 00:17:53 +0700

    fix email

--- src/main/java/com/project/emoney/utils/GlobalVariable.java
+++ src/main/java/com/project/emoney/utils/GlobalVariable.java
@@ -6,5 +6,5 @@ public class GlobalVariable {
   public final static int OTP_LIFETIME_MINUTES = 2;
   public final static int TRANSACTION_LIFETIME_HOURS = 24;
   public final static int EMAILTOKEN_LIFETIME_HOURS = 24;
-  public final static String HOST = "localhost:9706";
+  public final static String HOST = "https://be-emoney.herokuapp.com";
 }
