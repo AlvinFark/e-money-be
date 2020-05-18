7f279fab412eea267eada1b28996209e63ba4077
Alvin <alvin.farkhan@gmail.com>
2020-05-18 12:27:00 +0700

    emailtoken service test

--- src/test/java/com/project/emoney/service/EmailTokenServiceTest.java
+++ src/test/java/com/project/emoney/service/EmailTokenServiceTest.java
@@ -4,14 +4,18 @@ import com.project.emoney.entity.EmailToken;
 import com.project.emoney.entity.User;
 import com.project.emoney.mapper.EmailTokenMapper;
 import com.project.emoney.service.impl.EmailTokenServiceImpl;
+import com.project.emoney.utils.Generator;
 import com.project.emoney.utils.GlobalVariable;
+import com.project.emoney.utils.GmailSMTP;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.mail.MessagingException;
 import java.time.LocalDateTime;
 import static org.mockito.Mockito.*;
 
@@ -21,6 +25,12 @@ public class EmailTokenServiceTest {
 
   @Mock
   EmailTokenMapper emailTokenMapper;
+
+  @Mock
+  private Generator generator;
+
+  @Mock
+  private GmailSMTP gmailSMTP;
 
   @InjectMocks
   EmailTokenServiceImpl emailTokenService;
@@ -47,5 +57,21 @@ public class EmailTokenServiceTest {
     final User user = getUser();
     emailTokenService.insertByUserAndToken(user,emailToken.getToken());
     verify(emailTokenMapper, times(1)).insert(emailToken);
+  }
+
+  @Test
+  public void getEmailTokenByTokenTest() {
+    final EmailToken expected = getEmailToken();
+    final String token = "JYFA87AJFS8AFK2JD0A2";
+    when(emailTokenMapper.getByToken(token)).thenReturn(expected);
+    assert emailTokenService.getByToken(token).equals(expected);
+  }
+
+  @Test
+  public void sendEmailSuccessTest() {
+    final String token = "JYFA87AJFS8AFK2JD0A2";
+    final User user = getUser();
+    when(generator.generateToken()).thenReturn(token);
+    assert emailTokenService.sendEmail(user).equals("success");
   }
 }
